80113b290194beb6130daa195b8b44c8af673abd
juncaipeng <13006307475@163.com>
2022-06-07 11:14:41 +0800

    train portrait14k

--- contrib/PP-HumanSeg/run_train.sh
+++ contrib/PP-HumanSeg/run_train.sh
@@ -4,6 +4,11 @@ echo "tag: ${tag}"
 
 save_dir="output/${model}/${tag}"
 mkdir -p ${save_dir}
+cd ${save_dir}
+rm -rf log_dir
+rm -rf log_train.txt
+rm -rf *.log
+cd -
 
 echo "config: configs/${model}.yml"
 echo "save_dir: ${save_dir}"
@@ -13,7 +18,7 @@ nohup python -m paddle.distributed.launch --log_dir ${save_dir}/log_dir train.py
     --config configs/${model}.yml \
     --save_dir ${save_dir} \
     --save_interval 500 \
-    --num_workers 3 \
+    --num_workers 4 \
     --do_eval \
     --use_vdl \
     --log_iters 10 \
