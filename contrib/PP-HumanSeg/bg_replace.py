cb8a5c1f4a660163f2ba2793cf6c23633398a79e
juncaipeng <13006307475@163.com>
2022-06-14 19:20:50 +0800

    0614 1

--- contrib/PP-HumanSeg/bg_replace.py
+++ contrib/PP-HumanSeg/bg_replace.py
@@ -16,6 +16,9 @@
 import argparse
 import os
 import os.path as osp
+import sys
+__dir__ = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.abspath(os.path.join(__dir__, '../../')))
 
 import cv2
 import numpy as np
